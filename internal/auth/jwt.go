72740c95b9030838ebd1c5bcaf881c36927ff6d9
Firuz Chamarov <firuzchamarov@gmail.com>
2026-03-20 05:59:39 +0400

    jwt updates

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,9 +1,11 @@
 package auth
 
+// ~ go get github.com/golang-jwt/jwt/v5 - terminal command to install the JWT library
+
 import "github.com/golang-jwt/jwt/v5"
 
 type Claims struct {
-	UserID uint `json:"user_id"`
-	Email string `json:"email"`
+	UserID uint   `json:"user_id"`
+	Email  string `json:"email"`
 	jwt.RegisteredClaims
 }

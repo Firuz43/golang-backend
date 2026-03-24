22ef42087ecc9264dc8cc753650a21649d24c94a
Firuz Chamarov <firuzchamarov@gmail.com>
2026-03-24 22:50:53 +0400

    creating getproduct functionality

--- internal/handlers/product_handler.go
+++ internal/handlers/product_handler.go
@@ -1,8 +1,36 @@
 package handlers
 
-import "github.com/jmoiron/sqlx"
+import (
+	"encoding/json"
+	"net/http"
 
+	"github.com/Firuz43/ecommerce/internal/models"
+	"github.com/jmoiron/sqlx"
+)
+
+// ProductHandler will handle all product-related HTTP requests
 type ProductHandler struct {
-	// We will add the DB connection here later when we implement the methods
 	DB *sqlx.DB
+}
+
+// NewProductHandler is our "Constructor" for ProductHandler
+func NewProductHandler(db *sqlx.DB) *ProductHandler {
+	return &ProductHandler{DB: db}
+}
+
+// GetProducts returns all products in the database
+func (h *ProductHandler) GetProducts(w http.ResponseWriter, r *http.Request) {
+	var products []models.Product
+
+	// We want to return products sorted by newest first, so we order by created_at DESC
+	query := `SELECT * FROM products ORDER BY created_at DESC`
+	// sqlx's Select method will automatically map the rows to our Product struct slice
+	err := h.DB.Select(&products, query)
+	if err != nil {
+		http.Error(w, "Database error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
 }

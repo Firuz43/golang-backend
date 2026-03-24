bd921d9046674a19f75daeb7c1bda52e1576bcb3
Firuz Chamarov <firuzchamarov@gmail.com>
2026-03-24 22:35:32 +0400

    creating our product model

---
+++ internal/models/product.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Product struct {
+	ID          string    `json:"id" db:"id"`
+	Name        string    `json:"name" db:"name"`
+	Description string    `json:"description" db:"description"`
+	Price       float64   `json:"price" db:"price"`
+	Stock       int       `json:"stock" db:"stock"`
+	ImageURL    string    `json:"image_url" db:"image_url"`
+	CreatedAt   time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at" db:"updated_at"`
+}
